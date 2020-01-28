8cfbc41103ad364c763eccfce619536942f8818f
Juaka <juakacc@gmail.com>
2020-01-28 00:34:21 -0300

    Consumindo API

--- src/components/DisposicaoVeiculos.js
+++ src/components/DisposicaoVeiculos.js
@@ -2,19 +2,42 @@ import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 
 export default class DisposicaoVeiculos extends React.Component {
+
+    state = {
+        viagens: []
+    }
+
+    componentDidMount() {
+        this.carregarViagens()
+    }
+
+    carregarViagens = () => {
+        const options = {
+            method: 'GET'
+        }
+
+        fetch('http://192.168.31.20:8080/viagens/nao-concluidas', options)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    viagens: res
+                })
+            })
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.title}>Disposição atual dos veículos:</Text>
 
-                <Text style={styles.motorista}>Deusinho</Text>
-                <Text style={styles.veiculo}>- Ranger -</Text>
-                
-                <Text style={styles.motorista}>Romário</Text>
-                <Text style={styles.veiculo}>- Spin -</Text>
-                
-                <Text style={styles.motorista}>Zezinho</Text>
-                <Text style={styles.veiculo}>- Van -</Text>
+                {this.state.viagens.map(viagem => {
+                    return (
+                        <View key={viagem.id}>
+                            <Text style={styles.motorista}>{viagem.motorista.apelido}</Text>
+                            <Text style={styles.veiculo}>- {viagem.veiculo.nome} -</Text>
+                        </View>
+                    )
+                })}
             </View>
         )
     }
--- src/components/VeiculosSelect.js
+++ src/components/VeiculosSelect.js
@@ -2,16 +2,52 @@ import React from 'react'
 import {Picker, View, Text, StyleSheet} from 'react-native'
 
 
-export default props => {
-    return (
-        <View style={styles.container}>
-            <Text>Veículo:</Text>
-            <Picker>
-                <Picker.Item label='Gol02 - 40.000 KM' value='gol_01' />
-                <Picker.Item label='Gol01 - 40.000 KM' value='gol_02' />
-            </Picker>
-        </View>
-    )
+export default class VeiculoSelect extends React.Component {
+
+    state = {
+        veiculos: [],
+        selecionado: null
+    }
+
+    componentDidMount() {
+        this.getVeiculos()
+    }
+
+    getVeiculos = () => {
+        const options = {
+            method: 'GET'
+        }
+        fetch('http://192.168.31.20:8080/veiculos/disponiveis', options)
+            .then(res => res.json())
+            .then(res => {
+                this.setState(
+                    {
+                        veiculos: res,
+                        selecionado: res[0].id
+                    }
+                )
+            }
+            )
+            .catch(er => console.log(er.message)
+            )
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Veículo:</Text>
+                <Picker
+                    selectedValue={this.state.selecionado}
+                    onValueChange={(itemValue, itemIndex) => {
+                        this.setState({selecionado: itemValue})
+                    }}>
+                    {this.state.veiculos.map(item => {
+                        return (<Picker.Item label={item.nome} value={item.id} key={item.id}/>) 
+                    })}
+                </Picker>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
